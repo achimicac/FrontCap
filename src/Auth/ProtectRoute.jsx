29c2664a47b7dd27916667ba7e21ef026e45a2b8
achimicac <lagrongduke@gmail.com>
2024-05-03 21:48:51 +0700

    fix auth

--- src/Auth/ProtectRoute.jsx
+++ src/Auth/ProtectRoute.jsx
@@ -1,12 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import useAuth from './useAuth';
 
-export default function ProtectRoute({protectrole}) {
+export default function ProtectRoute() {
       const { auth } = useAuth()
 
       //return isAuthenticated ? <Outlet/> : <Navigate to="/login" />;
       return auth ?
-                  <Navigate to="/" />
+                  <Outlet />
                   :
                   <Navigate to="/login" />
 }
