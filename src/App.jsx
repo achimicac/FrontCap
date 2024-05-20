c5b082b334b43f2fa8061f251600d728604219e4
DisLife373 <keerataphant@gmail.com>
2024-05-20 14:18:59 +0700

    confused

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,164 @@
-import { BrowserRouter, Routes, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import Signup from './pages/signup';
-import Login from './pages/login';
-import Navbar from './components/Navbar';
-import StatusBar from './components/StatusBar';
-import MaidStatusWait from './pages/maid/wait';
-import MaidStatusWork from './pages/maid/work';
-import MaidStatusEnd from './pages/maid/end';
-import MaidMain from './pages/maid/main';
-import MaidProfile from './pages/maid/profile';
-import UserStatusWait from './pages/customer/wait';
-import UserStatusWork from './pages/customer/work';
-import UserStatusRating from './pages/customer/rating';
-import MaidProfileEdit from './pages/maid/profileEdit';
-import UserProfile from './pages/customer/profile';
-import UserProfileEdit from './pages/customer/profileEdit';
-import MaidOtherProfile from './pages/maid/otherProfile';
-import UserOtherProfile from './pages/customer/otherProfile';
-import UserMaidEmploy from './pages/customer/maidEmploy';
-import CustomerMain from './pages/customer/main';
-import ProtectRoute from './Auth/ProtectRoute';
-import roleAccess from './pages/roleAccess';
-import { AuthProvider } from './Auth/AuthProvider';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import Navbar from "./components/Navbar";
+import StatusBar from "./components/StatusBar";
+import MaidStatusWait from "./pages/maid/wait";
+import MaidStatusWork from "./pages/maid/work";
+import MaidStatusEnd from "./pages/maid/end";
+import MaidMain from "./pages/maid/main";
+import MaidProfile from "./pages/maid/profile";
+import UserStatusWait from "./pages/customer/wait";
+import UserStatusWork from "./pages/customer/work";
+import UserStatusRating from "./pages/customer/rating";
+import MaidProfileEdit from "./pages/maid/profileEdit";
+import UserProfile from "./pages/customer/profile";
+import UserProfileEdit from "./pages/customer/profileEdit";
+import MaidOtherProfile from "./pages/maid/otherProfile";
+import UserOtherProfile from "./pages/customer/otherProfile";
+import UserMaidEmploy from "./pages/customer/maidEmploy";
+import CustomerMain from "./pages/customer/main";
+import ProtectRoute from "./Auth/ProtectRoute";
+import roleAccess from "./pages/roleAccess";
+import { AuthProvider } from "./Auth/AuthProvider";
 //Component 3 หน้าของ Maid Status รูปแบบเหมือนกันเลย อาจจะมาแก้ให้เป็นใช้ component ร่วมกันไปเลย
 
 function App() {
-
-    const router = createBrowserRouter([{
-        path: '',
-        element: <roleAccess />
-    },{
-        path: 'signup',
-        element: <Signup />
-    },{
-        path: 'login',
-        element: <Login />
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <roleAccess />,
+    },
+    {
+      path: "signup",
+      element: <Signup />,
+    },
+    {
+      path: "login",
+      element: <Login />,
     },
     //Maid Routes
+    {
+      //element: <ProtectRoute />,
+      children: [
         {
-            //element: <ProtectRoute />,
-            children: [{
-                path: 'maid',
-                element: <Navbar />,
-                children: [{
-                        path: 'main',
-                        element: < MaidMain /> ,
-                    },{
-                        path: 'status',
-                        element: <StatusBar firstpage={'รอยืนยัน'} secondpage={'กำลังทำ'} thirdpage={'จบงาน'} />,
-                        children: [{
-                                path: 'wait',
-                                element: <MaidStatusWait />
-                            },{
-                                path: 'work',
-                                element: <MaidStatusWork />
-                            },{
-                                path: 'end',
-                                element: <MaidStatusEnd />
-                            }]
-                    },{
-                        path: 'profile',
-                        element: <MaidProfile />
-                    },{
-                        path: 'profile/edit',
-                        element: <MaidProfileEdit />
-                    },{
-                        path: 'customers/profile/:id',
-                        element: <MaidOtherProfile />
-                    }
-                ]
-            }]
+          path: "maid",
+          element: <Navbar />,
+          children: [
+            {
+              path: "main",
+              element: <MaidMain />,
+            },
+            {
+              path: "status",
+              element: (
+                <StatusBar
+                  firstpage={"รอยืนยัน"}
+                  secondpage={"กำลังทำ"}
+                  thirdpage={"จบงาน"}
+                />
+              ),
+              children: [
+                {
+                  path: "wait",
+                  element: <MaidStatusWait />,
+                },
+                {
+                  path: "work",
+                  element: <MaidStatusWork />,
+                },
+                {
+                  path: "end",
+                  element: <MaidStatusEnd />,
+                },
+              ],
+            },
+            {
+              path: "profile",
+              element: <MaidProfile />,
+            },
+            {
+              path: "profile/edit",
+              element: <MaidProfileEdit />,
+            },
+            {
+              path: "customers/profile/:id",
+              element: <MaidOtherProfile />,
+            },
+          ],
         },
-        //Customer route
+      ],
+    },
+    //Customer route
+    {
+      //element: <ProtectRoute />,
+      children: [
         {
-            //element: <ProtectRoute />,
-            children: [{
-                path: 'customer',
-                element: <Navbar />,
-                children: [{
-                        path: 'main',
-                        element: <CustomerMain />
-                    },{
-                        path: 'status',
-                        element: <StatusBar firstpage={'รอยืนยัน'} secondpage={'กำลังทำ'} thirdpage={'ให้คะแนน'} />,
-                        children: [{
-                                path: 'wait',
-                                element: <UserStatusWait />
-                            },{
-                                path: 'work',
-                                element: <UserStatusWork />
-                            },{
-                                path: 'end',
-                                element: <UserStatusRating />
-                            }]
-                    },{
-                        path: 'profile',
-                        element: <UserProfile />
-                    },{
-                        path: 'profile/edit',
-                        element: <UserProfileEdit />
-                    },{
-                        path: 'maids/profile/:id',
-                        element: <UserOtherProfile />
-                    },{
-                        path: 'maids/profile/:id/employ',
-                        element: <UserMaidEmploy />
-                    }
-                ]
-            }]
-        }
-        
-    ])
+          path: "customer",
+          element: <Navbar />,
+          children: [
+            {
+              path: "main",
+              element: <CustomerMain />,
+            },
+            {
+              path: "status",
+              element: (
+                <StatusBar
+                  firstpage={"รอยืนยัน"}
+                  secondpage={"กำลังทำ"}
+                  thirdpage={"ให้คะแนน"}
+                />
+              ),
+              children: [
+                {
+                  path: "wait",
+                  element: <UserStatusWait />,
+                },
+                {
+                  path: "work",
+                  element: <UserStatusWork />,
+                },
+                {
+                  path: "end",
+                  element: <UserStatusRating />,
+                },
+              ],
+            },
+            {
+              path: "profile",
+              element: <UserProfile />,
+            },
+            {
+              path: "profile/edit",
+              element: <UserProfileEdit />,
+            },
+            {
+              path: "maids/profile/:id",
+              element: <UserOtherProfile />,
+            },
+            {
+              path: "maids/profile/:id/employ",
+              element: <UserMaidEmploy />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
 
-    return (
-        <>
-            
-                <RouterProvider router={router} />
-            
-            
-        </>
-    )
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 //Test again
 
